9dbd8a53f50d783a4e5d4c73c10e370e25c87dd6
10gic <2391796+10gic@users.noreply.github.com>
2021-05-06 22:05:52 +0800

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
 
 setup(
     name='btc-address-dump',
-    version='0.1.9',
+    version='0.2.0',
     author='cig01',
     url='https://github.com/10gic/btc-address-dump',
     license='MIT License',
