f0e10f921e850095973f2be47274b5fe3769c2b3
10gic <2391796+10gic@users.noreply.github.com>
2021-05-08 19:31:45 +0800

    Force dependency pyyaml>=5.1

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ requirements = [
 
 setup(
     name='btc-address-dump',
-    version='0.2.7',
+    version='0.2.8',
     author='cig01',
     url='https://github.com/10gic/btc-address-dump',
     license='MIT License',
     description='A utility for dump btc address from mnemonic words or private key or public key',
     long_description=open('README.rst').read(),
-    install_requires=["ecdsa", "base58", "pyyaml"],
+    install_requires=["ecdsa>=0.15", "base58>=2.0.0", "pyyaml>=5.1"],
     python_requires='>=3',
     packages=['btc_address_dump'],
     include_package_data=True,
