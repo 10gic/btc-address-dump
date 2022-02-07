04c4e0617974dd8025c1f5d6567978fb1a207ca7
gruve-p <jackielove4u@hotmail.com>
2022-02-07 12:53:27 +0100

    Correct Groestlcoin parameters

--- btc_address_dump/p2sh_p2wpkh_util.py
+++ btc_address_dump/p2sh_p2wpkh_util.py
@@ -1,4 +1,5 @@
 import base58
+import base58grs
 import os
 import sys
 
@@ -23,3 +24,20 @@ def pubkey_to_p2sh_p2wpkh_addr(pubkey_compressed: bytes, version: bytes) -> byte
     pubkey_hash = common_util.sha256(pubkey_compressed)
     rip = common_util.ripemd160(pubkey_hash)
     return hash160_to_p2sh_p2wpkh_addr(rip, version)
+
+
+def grs_hash160_to_p2sh_p2wpkh_addr(hash160: bytes, version: bytes) -> bytes:
+    redeem_script = b'\x00\x14' + hash160  # 0x00: OP_0, 0x14: PushData
+    redeem_hash = common_util.sha256(redeem_script)
+    redeem_rip = common_util.ripemd160(redeem_hash)
+    # Base-58 encoding with a checksum
+    checksum = common_util.grs_base58_cksum(version + redeem_rip)
+    address = base58grs.b58encode(version + redeem_rip + checksum)
+    return address
+
+
+def grs_pubkey_to_p2sh_p2wpkh_addr(pubkey_compressed: bytes, version: bytes) -> bytes:
+    """ Derives p2sh-segwit (p2sh p2wpkh) address from pubkey """
+    pubkey_hash = common_util.sha256(pubkey_compressed)
+    rip = common_util.ripemd160(pubkey_hash)
+    return grs_hash160_to_p2sh_p2wpkh_addr(rip, version)
--- btc_address_dump/wif_util.py
+++ btc_address_dump/wif_util.py
@@ -1,5 +1,7 @@
 import hashlib
+import groestlcoin_hash
 import base58
+import base58grs
 from typing import Union
 
 
@@ -41,6 +43,29 @@ def encode_wif(private_key: Union[str, bytes], version: bytes, compressed_wif: b
     return base58.b58encode(hash_bytes)
 
 
+def grs_encode_wif(private_key: Union[str, bytes], version: bytes, compressed_wif: bool = False) -> bytes:
+    """ Encode wif format private key """
+    private_key = scrub_input(private_key)
+
+    # prepended version byte to private key
+    private_key_with_version = version + private_key
+    if compressed_wif:
+        private_key_with_version = version + private_key + b'\x01'
+    # perform DOUBLE GROESTL-512 hash on the mainnet_private_key
+    hash_bytes = groestlcoin_hash.getHash(private_key_with_version, len(private_key_with_version))
+
+    # create a checksum using the first 4 bytes of the GROESTL-512 hash
+    # append the 4 checksum bytes to the mainnet_private_key
+    checksum = hash_bytes[:4]
+
+    # print('checksum', checksum.hex())
+    hash_bytes = private_key_with_version + checksum
+    # print('hash', hash_bytes.hex())
+
+    # convert private_key_with_version + checksum into base58 encoded string
+    return base58grs.b58encode(hash_bytes)
+
+
 def decode_wif(wif: str, compressed_wif: bool = False) -> bytes:
     """ Decode wif format private key """
     decoded = base58.b58decode(wif)
@@ -51,10 +76,31 @@ def decode_wif(wif: str, compressed_wif: bool = False) -> bytes:
     return private_key
 
 
-def is_valid_wif(wif: str) -> bool:
+def grs_decode_wif(wif: str, compressed_wif: bool = False) -> bytes:
+    """ Decode wif format private key """
+    decoded = base58grs.b58decode(wif)
+    if len(decoded) == 38 or len(decoded) == 37:
+        private_key = decoded[1:33]  # WIF-compressed: [0x80 xxxx 0x01 checksum] or WIF: [0x80 xxxx checksum]
+    else:
+        raise ValueError("invalid WIF")
+    return private_key
+
+
+def is_valid_wif(wif: str, chain: str) -> bool:
     """ Check if wif is valid"""
     try:
-        base58.b58decode_check(wif)
-        return True
+        if chain == "grs" or chain == "grs_testnet":
+            result = base58grs.b58decode(wif)
+            #print(result)
+            result, check = result[:-4], result[-4:]
+            # DOUBLE GROESTL-512 hash
+            digest = groestlcoin_hash.getHash(result, len(result))
+        else:
+            result = base58.b58decode(wif)
+            #print(result)
+            result, check = result[:-4], result[-4:]
+            # DOUBLE sha256 hash
+            digest = hashlib.sha256(hashlib.sha256(result).digest()).digest()
+        return check == digest[:4]
     except ValueError:
         return False
